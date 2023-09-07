a7c0525e623925cb82d91320e2a317e2e326d16a
sechen12 <schen12@charlottesvilleschools.org>
2023-09-07 10:33:03 -0400

    comments

--- raspberry-pi/LED_blink.py
+++ raspberry-pi/LED_blink.py
@@ -2,11 +2,11 @@ import board
 import time
 import digitalio
 
-led = digitalio.DigitalInOut(board.LED)
+led = digitalio.DigitalInOut(board.LED) #defines LED
 led.direction = digitalio.Direction.OUTPUT
 
 while True:
-    led.value = True
-    time.sleep(0.5)
-    led.value = False
-    time.sleep(0.5)
+    led.value = True #turns LED on
+    time.sleep(0.5) #pause
+    led.value = False #turns LED off
+    time.sleep(0.5) #pause
