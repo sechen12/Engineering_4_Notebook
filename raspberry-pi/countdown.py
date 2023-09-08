61d80b2e766a0795bc3f5124ccb1d7933c3238ea
sechen12 <schen12@charlottesvillschools.org>
2023-09-08 10:41:16 -0400

    done

--- raspberry-pi/countdown.py
+++ raspberry-pi/countdown.py
@@ -3,10 +3,7 @@ import time
 import board
 import digitalio
 
-sec = 10
-
-while True:
-    sec = sec - 1
-    print(sec)
-    if sec < 1
-        print("lift off")
+for x in range(10):
+    print(10-x) #prints 10 --> 1 in descending order
+    time.sleep(1) #delays the count by one second in between numbers
+print("liftoff!") #print liftoff when the rest of the code is finished running
