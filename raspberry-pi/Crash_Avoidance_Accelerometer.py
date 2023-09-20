e4f338ad7a98aaa722f0e03eef93442d707dda93
sechen12 <schen12@charlottesvillschools.org>
2023-09-20 09:36:22 -0400

    done done

--- raspberry-pi/Crash_Avoidance_Accelerometer.py
+++ raspberry-pi/Crash_Avoidance_Accelerometer.py
@@ -5,14 +5,14 @@ import board
 import time
 import digitalio
 
-sda_pin = board.GP14
-scl_pin = board.GP15
-i2c = busio.I2C(scl_pin, sda_pin)
-mpu = adafruit_mpu6050.MPU6050(i2c)
+sda_pin = board.GP14 # defining serial data pin
+scl_pin = board.GP15 # defining serial clock pin
+i2c = busio.I2C(scl_pin, sda_pin) # allowing the sda and scl pins to communicate using one channel
+mpu = adafruit_mpu6050.MPU6050(i2c) # initializes the sensor
 
 
 while True:
-    print(f"x accelteration: {mpu.acceleration[0]}.")
+    print(f"x accelteration: {mpu.acceleration[0]}.") # this is an 'f-string'; it's a certain syntax that helps print the variables clearer
     print(f"y accelteration: {mpu.acceleration[1]}.")
     print(f"z accelteration: {mpu.acceleration[2]}.")
     print("")
