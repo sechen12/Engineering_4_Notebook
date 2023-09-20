c31584490fee7f07d87561ab8d3673e7c8024ca6
sechen12 <schen12@charlottesvillschools.org>
2023-09-20 09:18:49 -0400

    done

--- raspberry-pi/Crash_Avoidance_Accelerometer.py
+++ raspberry-pi/Crash_Avoidance_Accelerometer.py
@@ -10,8 +10,10 @@ scl_pin = board.GP15
 i2c = busio.I2C(scl_pin, sda_pin)
 mpu = adafruit_mpu6050.MPU6050(i2c)
 
+
 while True:
-    print(f"x Angular Velocity: {}.")
-    print(f"y Angular Velocity: {}.")
-    print(f"z Angular Velocity: {}.")
+    print(f"x accelteration: {mpu.acceleration[0]}.")
+    print(f"y accelteration: {mpu.acceleration[1]}.")
+    print(f"z accelteration: {mpu.acceleration[2]}.")
+    print("")
     time.sleep(.5)
